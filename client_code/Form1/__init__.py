3884da958b7a47b06fcc7de2bea317cadc27561b
hanafikds825@gmail.com <hanafikds825@gmail.com>
2024-06-05 05:54:32 +0000

    Created ServerModule1; Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,18 +1,27 @@
 from ._anvil_designer import Form1Template
 from anvil import *
-
+import anvil.server
 
 class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-    # Any code you write here will run before the form opens.
+    # Properti untuk menyimpan file yang diunggah
+    self.uploaded_file = None
 
   def file_loader_1_change(self, file, **event_args):
     """This method is called when a new file is loaded into this FileLoader"""
-    pass
+    # Simpan file yang diunggah ke properti
+    self.uploaded_file = file
+    # Tampilkan nama file di label_2
+    self.label_2.text = f"File uploaded: {file.name}"
 
   def button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
-    pass
+    if self.uploaded_file:
+      # Jika file telah diunggah, tampilkan nama file di label_2
+      self.label_2.text = f"File loaded: {self.uploaded_file.name}"
+    else:
+      # Jika tidak ada file yang diunggah, tampilkan pesan di label_2
+      self.label_2.text = "No file uploaded"
---
+++ server_code/ServerModule1.py
@@ -0,0 +1,16 @@
+import anvil.server
+import numpy as np
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import load_model
+
+# Load model once when the server module is loaded
+model = load_model('rice_disease_weights.h5')
+class_labels = {0: 'Bacterial leaf blight', 1: 'Brown spot', 2: 'Leaf smut'}
+
+@anvil.server.callable
+def predict_disease(img):
+    img = image.img_to_array(img)
+    img = np.expand_dims(img, axis=0) / 255.0  # Normalize
+    predictions = model.predict(img)
+    predicted_class = np.argmax(predictions)
+    return class_labels.get(predicted_class, 'Unknown class')
